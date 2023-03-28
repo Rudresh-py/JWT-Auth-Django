3cfbb9f4fa5e19f19896806b08131717bae58e96
Rudreshcg <rudresha.gangaswamaiah@softsuave.com>
2023-03-28 13:00:44 +0530

    updated login, user, logout views

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from .serializers import UserSerializer
 from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
 from .models import User
-# import jwt
+import jwt, datetime
 
 
 class RegisterView(APIView):
@@ -15,6 +15,7 @@ class RegisterView(APIView):
 
 
 class LoginView(APIView):
+
     def post(self, request):
         email = request.data['email']
         password = request.data['password']
@@ -26,6 +27,45 @@ class LoginView(APIView):
 
         if not user.check_password(password):
             raise AuthenticationFailed('Incorrect password')
-        return Response({
+        payload = {
+            'id': user.id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+            'iat': datetime.datetime.utcnow()
+        }
+
+        token = jwt.encode(payload, 'secret', algorithm='HS256')
+
+        response = Response()
+        response.set_cookie(key='jwt', value=token, httponly=True)
+        response.data = {
+            'jwt': token
+        }
+        return response
+
+
+class UserView(APIView):
+
+    def get(self, request):
+        token = request.COOKIES.get('jwt')
+
+        if not token:
+            raise AuthenticationFailed('Unauthenticated!')
+
+        try:
+            payload = jwt.decode(token, 'secret', algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('Unauthenticated!')
+
+        user = User.objects.filter(id=payload['id']).first()
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+
+class LogoutView(APIView):
+    def post(self, request):
+        response = Response()
+        response.delete_cookie('jwt')
+        response.data = {
             'message': 'success'
-        })
+        }
+        return response
